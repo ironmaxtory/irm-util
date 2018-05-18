41a8bbcbd4bac41d47d96b95e02feadaea070021
ironmax <ironmax@163.com>
2018-05-18 17:39:33 +0800

    U Upgrade util_recurse.js

--- src/utils/util_recurse.js
+++ src/utils/util_recurse.js
@@ -18,18 +18,19 @@ function UtilRecurse (tree, childKey) {
 UtilRecurse.prototype.subscribe = function(listener){
   let listeners = this.getListeners();
   listeners.push(listener);
+  return this;
 };
 
-UtilRecurse.prototype.recurseNode = function(tree, childKey){
+UtilRecurse.prototype.recurseNode = function(tree, childKey, lv){
   if (typeof tree !== 'object') { return false; }
 
   if (!tree[childKey] || (tree[childKey].length===0)) { return false; }
 
-  tree[childKey].map((item)=>{
+  tree[childKey].map((item, idx, items)=>{
     this.getListeners().map((listener)=>{
-      listener(item);
+      listener(item, idx, items, lv, tree);
     });
-    this.recurseNode(item, childKey);
+    this.recurseNode(item, childKey, lv+1);
   });
 };
 
